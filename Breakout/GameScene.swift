47d05f2a37a0a81ac6ed277c9bafdd801214a719
Michael Essex <123messex@gmail.com>
2021-03-30 10:30:28 -0500

    Made brick single layer

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -16,9 +16,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var playingGame = false
     var score = 0
     var lives = 3
+    var removedBricks = 0
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
-    var brick = SKSpriteNode()
+    var bricks = [SKSpriteNode]()
     var loseZone = SKSpriteNode()
     
     override func didMove(to view: SKView) {
@@ -59,16 +60,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node?.name == "brick" ||
-            contact.bodyB.node?.name == "brick" {
-            gameOver(winner:true)
-            brick.removeFromParent()
-            ball.removeFromParent()
+        // Ask each brick, "is it you?"
+        for brick in bricks {
+            if contact.bodyA.node == brick ||
+                contact.bodyB.node == brick {
+                score += 1
+                updateLabels()
+                brick.removeFromParent()
+                removedBricks += 1
+                if removedBricks == bricks.count {
+                    gameOver(winner: true)
+                }
+            }
         }
         if contact.bodyA.node?.name == "loseZone" ||
             contact.bodyB.node?.name == "loseZone" {
-            gameOver(winner:false)
-            ball.removeFromParent()
+            lives -= 1
+            if lives > 0 {
+                score = 0
+                resetGame()
+                kickBall()
+            }
+            else {
+                gameOver(winner: false)
+            }
         }
     }
     
@@ -77,7 +92,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //This stuff happens before each game starts
         makeBall()
         makePaddle()
-        makeBrick()
+        makeBricks()
         makeLoseZone()
     }
     func kickBall() {
@@ -141,55 +156,75 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(paddle)
     }
     
-    func makeBrick() {
-        brick.removeFromParent()
-        brick = SKSpriteNode (color: .blue, size: CGSize(width: 50, height: 20))
-        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
-        brick.name = "Brick"
+    // Helper function used to make each brick
+    func makeBrick(x: Int, y: Int, color: UIColor){
+        let brick = SKSpriteNode (color: color, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: x, y: y)
         brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
         brick.physicsBody?.isDynamic = false
         addChild(brick)
+        bricks.append(brick)
     }
-    
-    func makeLoseZone() {
-        loseZone = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
-        loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
-        loseZone.name = "loseZone"
-        loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
-        loseZone.physicsBody?.isDynamic = false
-        addChild(loseZone)
-    }
-    
-    func makeLabels() {
-        playLabel.fontSize = 24
-        playLabel.text = "Tap to start"
-        livesLabel.fontName = "Arial"
-        playLabel.position = CGPoint(x: frame.midX, y: frame.midY - 50)
-        playLabel.name = "playLabel"
-        addChild(playLabel)
-        
-        livesLabel.fontSize = 18
-        livesLabel.fontColor = .black
-        livesLabel.position = CGPoint(x: frame.minX + 50, y: frame.minY + 18)
-        addChild(livesLabel)
-        
-        scoreLabel.fontSize = 18
-        scoreLabel.fontColor = .black
-        scoreLabel.fontName = "Arial"
-        scoreLabel.position = CGPoint(x: frame.maxX - 50, y: frame.minY + 18)
-        addChild(scoreLabel)
-    }
-    
-    func gameOver(winner: Bool) {
-        playingGame = false
-        playLabel.alpha = 1
-        resetGame()
-        if winner {
-            playLabel.text = "You win! Tap to play again."
-        }
-        else {
-            playLabel.text = "Your bad! Tap to play again."
-        }
-    }
-    
 }
+
+func makeLoseZone() {
+    loseZone = SKSpriteNode(color: .red size: CGSize(width: frame.width, height: 50))
+    loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
+    loseZone.name = "loseZone"
+    loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+    loseZone.physicsBody?.isDynamic = false
+    addChild(loseZone)
+}
+
+func makeBricks() {
+    // First, remove any left over bricks (from prior game)
+    for brick in bricks {
+        if brick.parent != nil {
+            brick.removeFromParent()
+        }
+    }
+    bricks.removeAll() // Clear the array
+    removedBricks = 0 // Reset the counter
+    
+    // Now, figure the number and spacing of each row of bricks
+    let count = Int(frame.width) / 55 // Bricks per row
+    let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int(frame.minX) + 25
+    let y = Int(frame.maxY) - 15
+    for i in 0..<count {
+        let x = i * 55 + xOffset
+        makeBricks(x: x, y: y, color: .green)
+    }
+}
+func makeLabels() {
+    playLabel.fontSize = 24
+    playLabel.text = "Tap to start"
+    livesLabel.fontName = "Arial"
+    playLabel.position = CGPoint(x: frame.midX, y: frame.midY - 50)
+    playLabel.name = "playLabel"
+    addChild(playLabel)
+    
+    livesLabel.fontSize = 18
+    livesLabel.fontColor = .black
+    livesLabel.position = CGPoint(x: frame.minX + 50, y: frame.minY + 18)
+    addChild(livesLabel)
+    
+    scoreLabel.fontSize = 18
+    scoreLabel.fontColor = .black
+    scoreLabel.fontName = "Arial"
+    scoreLabel.position = CGPoint(x: frame.maxX - 50, y: frame.minY + 18)
+    addChild(scoreLabel)
+}
+
+func gameOver(winner: Bool) {
+    playingGame = false
+    playLabel.alpha = 1
+    resetGame()
+    if winner {
+        playLabel.text = "You win! Tap to play again."
+    }
+    else {
+        playLabel.text = "You're bad! Tap to play again."
+    }
+}
+
+
